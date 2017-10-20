9ebc13f43748168a5b734b82c123db591a091a95
usuario <usuario@10.22.148.52>
2017-10-20 11:56:58 +0100

    Kata3 versión3: se inserta el dominio una única vez, especificando el número de veces

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -5,23 +5,10 @@ public class Kata3 {
 
     public static void main(String[] args) {
         Histogram histogram = new Histogram();
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
+        histogram.increment("gmail.com", 7);
+        histogram.increment("ulpgc.es",2);
+        histogram.increment("ull.es",3);
+        histogram.increment("hotmail.com",10);
         
         new HistogramDisplay(histogram).execute();
     }
